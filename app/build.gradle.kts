22d92177bc22aafad6e4556dd0035b47951fb417
YashKudalkar05 <81027078+YashKudalkar05@users.noreply.github.com>
2025-04-08 22:04:35 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    //implementation ("com.journeyapps:zxing-android-embedded:4.3.0") // Add this line
 
 
 
